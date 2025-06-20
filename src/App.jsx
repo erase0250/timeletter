9371ef01c14f37ff6299f5970a0813c0c722f6c0
Jiwoo <erase0250@gmail.com>
2025-06-20 17:24:32 +0900

    feat: 홈 및 작성페이지 라우터 경로 설정 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import LetterWrite from "./pages/LetterWrite";
 
 function App() {
     return (
-        <div className="min-h-screen flex justify-center bg-white">
-            <div className="w-[390px] min-h-screen shadow-lg">
-                <Home />
+        <div className="min-h-screen flex justify-center bg-[#F8F1E8]">
+            <div className="w-[420px] min-h-screen shadow-lg">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/write" element={<LetterWrite />} />
+                </Routes>
             </div>
         </div>
     );
