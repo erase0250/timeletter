a13d99091dab1cbf1260e24aef5b2b977000c7b8
Jiwoo <erase0250@gmail.com>
2025-06-24 19:20:36 +0900

    feat: 편지 목록 테스트용 더미데이터 추가

---
+++ src/data/dummyLetters.js
@@ -0,0 +1,61 @@
+const dummyLetters = [
+    {
+        id: 1,
+        title: "제목제목",
+        content:
+            "내용내용내용\n두 번째 줄입니다.내용내용내용내용내용내용내용내용내용111내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용",
+        createdAt: "2025.05.16",
+        openAt: "2025.06.16",
+        isLock: true,
+    },
+    {
+        id: 2,
+        title: "제목제목222",
+        content: "내용내용내용\n첫 번째 줄입니다.\n두 번째 줄입니다.",
+        createdAt: "2025.05.18",
+        openAt: "2025.06.18",
+        isLock: false,
+    },
+    {
+        id: 3,
+        title: "제목제목333",
+        content: "내용내용내용\n첫 번째 줄입니다.\n두 번째 줄입니다.",
+        createdAt: "2023.05.18",
+        openAt: "2023.06.18",
+        isLock: true,
+    },
+    {
+        id: 4,
+        title: "제목제목444",
+        content: "내용내용내용\n첫 번째 줄입니다.\n두 번째 줄입니다.",
+        createdAt: "2024.05.18",
+        openAt: "2024.06.18",
+        isLock: true,
+    },
+    {
+        id: 5,
+        title: "제목제목555",
+        content: "내용내용내용\n첫 번째 줄입니다.\n두 번째 줄입니다.",
+        createdAt: "2025.09.18",
+        openAt: "2025.10.18",
+        isLock: false,
+    },
+    {
+        id: 6,
+        title: "제목제목666",
+        content: "내용내용내용\n첫 번째 줄입니다.\n두 번째 줄입니다.",
+        createdAt: "2025.09.18",
+        openAt: "2025.10.18",
+        isLock: false,
+    },
+    {
+        id: 7,
+        title: "제목제목777",
+        content: "내용내용내용\n첫 번째 줄입니다.\n두 번째 줄입니다.",
+        createdAt: "2025.09.18",
+        openAt: "2025.10.18",
+        isLock: false,
+    },
+];
+
+export default dummyLetters;
